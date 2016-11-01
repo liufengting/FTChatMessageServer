76b6c166fbae1f9211398b38fb4bbadb0b670464
liufengting <wo157121900@me.com>
2016-11-01 10:55:31 +0800

    initial commit

---
+++ FTChatMessageServer/FTChatMessageServer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FTChatMessageServer
+//
+//  Created by liufengting on 2016/11/1.
+//  Copyright © 2016年 LiuFengting. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FTChatMessageServer/FTChatMessageServer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FTChatMessageServer
+//
+//  Created by liufengting on 2016/11/1.
+//  Copyright © 2016年 LiuFengting. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
