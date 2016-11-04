c7d4a2c6244b372b4fce418c8bbbd578551c20fb
liufengting <wo157121900@me.com>
2016-11-04 17:03:49 +0800

    edit

--- FTChatMessageServer/Package.swift
+++ FTChatMessageServer/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name : "FTChatMessageServer",
     targets: [
-        Target(name: "FTChatMessageServer")
+//        Target(name: "FTChatMessageServer")
     ],
     dependencies: [
         .Package(url:"https://github.com/PerfectlySoft/Perfect-WebSockets.git", majorVersion: 2, minor: 0),
